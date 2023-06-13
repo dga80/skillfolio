067cdaaf29196fd2d2548e3343646399693c11bf
dga80 <dani.dga@gmail.com>
2023-06-13 12:40:35 +0200

    6th commit

---
+++ navbar/MobileNavigation.js
@@ -0,0 +1,11 @@
+import NavLinks from './NavLinks'
+import classes from '.NavBar.module.css'
+const MobileNavigation = () => {
+    return ( 
+        <nav className={classes.MobileNavigation}>
+            <NavLinks />
+        </nav>
+     );
+}
+ 
+export default MobileNavigation;
---
+++ navbar/NavBar.jsx
@@ -0,0 +1,14 @@
+import classes from '.NavBar.module.css'
+import MobileNavigation from './MobileNavigation';
+import Navigation from './Navigation';
+
+const  NavBar= () => {
+    return ( 
+        <div className={classes.NavBar}>
+            <Navigation />
+            <MobileNavigation />
+        </div>
+    );
+}
+ 
+export default NavBar;
